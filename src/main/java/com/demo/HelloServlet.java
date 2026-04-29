7f07ac15bb031018187d0daf17e19415b7374349
Balakrishnan Chinnathambi <48944261+Balakrishnan2k17@users.noreply.github.com>
2026-04-29 18:20:36 +0530

    Create HelloServlet.java

---
+++ src/main/java/com/demo/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        response.getWriter().println("<h1>Welcome to DevOps Demo App</h1>");
+        response.getWriter().println("<h2>CI/CD using GitHub Actions</h2>");
+        response.getWriter().println("<p>Instructor: Balakrishnan</p>");
+    }
+}
